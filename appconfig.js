7c65f1c0943b550ae79c4872630af366f41adda0
dimukko <dimukko@gmail.com>
2020-04-14 13:49:16 +0700

    Поменял базу данных

--- appconfig.js
+++ appconfig.js
@@ -1,6 +1,6 @@
 // Настройки приложения
 const NODE_ENV = process.env.NODE_ENV || 'develop';
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/mestodb';
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/newsdb';
 const PORT = process.env.PORT || 3000;
 const JWT_SECRET = NODE_ENV === 'production' ? process.env.JWT_SECRET : 'ABRAKAdDDaaabRAA';
 
