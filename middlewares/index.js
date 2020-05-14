2ef6e156152506a2174c02dcb71c070a7afdc5cb
dimukko <dimukko@gmail.com>
2020-05-14 21:37:01 +0700

    CORS

--- middlewares/index.js
+++ middlewares/index.js
@@ -4,10 +4,13 @@ const limiter = require('./limiter');
 const helmet = require('./helmet');
 const bodyParser = require('./body-parser');
 const cookieParser = require('./cookie-parser');
+const cors = require('./cors');
 
+router.use(cors);
 router.use(limiter);
 router.use(helmet);
 router.use(bodyParser);
 router.use(cookieParser);
+
 
 module.exports = router;
